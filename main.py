efd113c9f8c1ec18dc0702e65919610a230324ef
eliaswasem <ewasem@outlook.de>
2026-04-28 17:14:45 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,155 @@
+import sys
+import os
+
+from PySide6.QtCore import Qt
+from PySide6.QtWidgets import QApplication, QMainWindow, QPushButton,QVBoxLayout, QWidget, QLineEdit, \
+    QGridLayout
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle("Calculator")
+
+        centralW = QWidget()
+        self.setCentralWidget(centralW)
+
+        mainLayout = QVBoxLayout(centralW)
+
+        # Preview and Output Window
+        self.display = QLineEdit()
+        self.display.setReadOnly(True)
+        self.display.setFocusPolicy(Qt.NoFocus)
+        self.display.setAlignment(Qt.AlignRight)
+        mainLayout.addWidget(self.display)
+
+        # Grid
+        grid = QGridLayout()
+        mainLayout.addLayout(grid)
+
+        # Buttons
+        self.b0 = QPushButton("0")
+        self.b1 = QPushButton("1")
+        self.b2 = QPushButton("2")
+        self.b3 = QPushButton("3")
+        self.b4 = QPushButton("4")
+        self.b5 = QPushButton("5")
+        self.b6 = QPushButton("6")
+        self.b7 = QPushButton("7")
+        self.b8 = QPushButton("8")
+        self.b9 = QPushButton("9")
+
+        self.btimes = QPushButton("*")
+        self.bdivided = QPushButton("/")
+        self.bplus = QPushButton("+")
+        self.bminus = QPushButton("-")
+
+        self.bequal = QPushButton("=")
+        self.bdot = QPushButton(".")
+        self.bc = QPushButton("C")
+        self.bb = QPushButton("<-")
+
+        self.blb = QPushButton("(")
+        self.brb = QPushButton(")")
+
+        # adding the buttons to the grid
+        grid.addWidget(self.bb, 0 , 0)
+        grid.addWidget(self.blb, 0, 1)
+        grid.addWidget(self.brb, 0, 2)
+        grid.addWidget(self.bc, 0, 3)
+
+        grid.addWidget(self.b7, 1, 0)
+        grid.addWidget(self.b8, 1, 1)
+        grid.addWidget(self.b9, 1, 2)
+        grid.addWidget(self.bdivided, 1, 3)
+
+        grid.addWidget(self.b4, 2, 0)
+        grid.addWidget(self.b5, 2, 1)
+        grid.addWidget(self.b6, 2, 2)
+        grid.addWidget(self.btimes, 2, 3)
+
+        grid.addWidget(self.b1, 3, 0)
+        grid.addWidget(self.b2, 3, 1)
+        grid.addWidget(self.b3, 3, 2)
+        grid.addWidget(self.bminus, 3, 3)
+
+        grid.addWidget(self.b0, 4, 0)
+        grid.addWidget(self.bdot, 4, 1)
+        grid.addWidget(self.bequal, 4, 2)
+        grid.addWidget(self.bplus, 4, 3)
+
+        # ------------------------ Logic ------------------------ #
+
+        # Initialize calculator variables
+        self.calculation = ""
+        self.output = ""
+
+        # function assignment
+        self.b0.clicked.connect(lambda: self.add_symbol("0"))
+        self.b1.clicked.connect(lambda: self.add_symbol("1"))
+        self.b2.clicked.connect(lambda: self.add_symbol("2"))
+        self.b3.clicked.connect(lambda: self.add_symbol("3"))
+        self.b4.clicked.connect(lambda: self.add_symbol("4"))
+        self.b5.clicked.connect(lambda: self.add_symbol("5"))
+        self.b6.clicked.connect(lambda: self.add_symbol("6"))
+        self.b7.clicked.connect(lambda: self.add_symbol("7"))
+        self.b8.clicked.connect(lambda: self.add_symbol("8"))
+        self.b9.clicked.connect(lambda: self.add_symbol("9"))
+
+        self.bplus.clicked.connect(lambda: self.add_symbol("+"))
+        self.bminus.clicked.connect(lambda: self.add_symbol("-"))
+        self.btimes.clicked.connect(lambda: self.add_symbol("*"))
+        self.bdivided.clicked.connect(lambda: self.add_symbol("/"))
+
+        self.blb.clicked.connect(lambda: self.add_symbol("("))
+        self.brb.clicked.connect(lambda: self.add_symbol(")"))
+
+        self.bequal.clicked.connect(self.calculate)
+        self.bdot.clicked.connect(lambda: self.add_symbol("."))
+        self.bc.clicked.connect(self.clear)
+        self.bb.clicked.connect(self.backspace)
+
+    def add_symbol(self, symbol):
+        self.calculation += symbol
+        self.display.setText(self.calculation)
+
+    def calculate(self):
+        operators = ["+","-","/","*"]
+        error = ""
+
+        if any(operator in self.calculation for operator in operators):
+            if "+" in self.calculation or "-" in self.calculation or "/" in self.calculation or "*" in self.calculation:
+                try:
+                    self.output = eval(self.calculation)
+                except Exception as e:
+                    error = e
+                    self.calculation = ""
+                if error == "":
+                    self.display.setText(f"{self.calculation} = {self.output}")
+                else:
+                    self.display.setText(f"Error: {str(error)}")
+
+    def clear (self):
+        self.calculation = ""
+        self.display.setText("")
+
+    def backspace(self):
+        self.calculation = self.calculation[:-1]
+        self.display.setText(self.calculation)
+
+
+app = QApplication(sys.argv)
+
+if getattr(sys, 'frozen', False):
+    base_path = sys._MEIPASS
+else:
+    base_path = os.path.dirname(__file__)
+qss_path = os.path.join(base_path, "style.qss")
+
+with open(qss_path, "r") as f:
+    app.setStyleSheet(f.read())
+window = MainWindow()
+window.show()
+
+app.exec()
